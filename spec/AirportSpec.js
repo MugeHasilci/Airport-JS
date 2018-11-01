289d3bd85cfc60179fd58e0859f466a283c6260a
muge <muge.hasilci@hotmail.com>
2018-11-01 17:23:12 +0000

    instructs a plane to land at an airport

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,1 +1,17 @@
 'use strict';
+
+describe('Airport', function(){
+  var airport;
+  var plane;
+
+  beforeEach(function(){
+    airport = new Airport;
+  });
+
+  describe('.land', function(){
+    it('instructs a plane to land at an airport', function(){
+      plane = jasmine.createSpy('plane');
+      expect(airport.land(plane)).toEqual(plane);
+    });
+  });
+});
---
+++ src/Airport.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function Airport(){
+};
+
+Airport.prototype.land = function(plane) {
+  return plane;
+}
