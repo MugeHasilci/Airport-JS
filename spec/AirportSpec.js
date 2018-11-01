1bdd9c54d2003a3888590742c44c0385a11047d8
muge <muge.hasilci@hotmail.com>
2018-11-01 21:49:55 +0000

    weather condition added

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,27 +1,59 @@
 'use strict';
 
 describe('Airport', function(){
+
   var airport;
   var plane;
 
-  beforeEach(function(){
-    airport = new Airport;
+
+  beforeEach(function() {
+    airport = new Airport();
     plane = jasmine.createSpy('plane');
   });
 
-  describe('.planes', function(){
+  describe('.planes', function() {
     describe("after a plane is landed", function() {
       it('returns an array of planes', function(){
+        spyOn(airport, '_isStormy').and.returnValue(false);
         airport.land(plane);
         expect(airport.planes).toEqual([plane]);
       });
     });
+  });
     describe('after plane takes off', function(){
       it('it returns and empty array', function() {
+        spyOn(airport, '_isStormy').and.returnValue(false)
         airport.land(plane);
         airport.takeOff(plane);
         expect(airport.planes.length).toEqual(0);
       });
     });
+
+
+    describe("._isStormy", function() {
+      it('returns a weather condition', function() {
+        spyOn(airport, '_isStormy').and.returnValue(true);
+        expect(airport._isStormy()).toBe(true);
+      });
+    });
+
+    describe('.takeoff', function() {
+      describe('cannot take off when the weather is stormy', function(){
+      it('throws an error if stormy', function(){
+        spyOn(airport, '_isStormy').and.returnValue(false);
+        airpot.land(plane);
+        spyOn(airport, '_isStormy').and.returnValue('true');
+        expect( function(){ airport.takeOff(); }).toThrowError('Weather is too stormy to take off');
+      });
+    });
   });
+
+  describe(".land", function() {
+   describe("when its stormy", function() {
+     it('returns an error', function() {
+       spyOn(airport, '_isStormy').and.returnValue(true);
+       expect( function(){ airport.land(); } ).toThrowError('Weather is too stormy to land');
+     });
+   });
+ });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -2,12 +2,24 @@
 
 function Airport(){
   this.planes = [];
-};
+}
 
 Airport.prototype.land = function(plane) {
+  if (this._isStormy()) {
+    throw new Error('Weather is too stormy to land');
+  } else {
   this.planes.push(plane);
+}
 };
 
 Airport.prototype.takeOff = function() {
-  this.planes.pop();
+  if (this._isStormy()) {
+    throw new Error('Weather is too stormy to take off');
+  } else {
+    this.planes.pop();
+  }
+};
+
+Airport.prototype._isStormy = function() {
+  (Math.random(1) < 0.5);
 };
